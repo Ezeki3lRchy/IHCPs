d9a37bb24e9b5b55e62f03158a570f50d52e4f39
Brian <108606033+Lenjis@users.noreply.github.com>
2024-12-06 11:22:52 +0800

    Update FastRename.py
    Add rename process

--- FastRename.py
+++ FastRename.py
@@ -35,7 +35,8 @@ def RemoveIndex(path="./"):
         for filename in os.listdir(path):
             match = re.search(pattern, filename)
             if match:
-                newname = filename.replace(match.group(), "")
+                newname = filename.replace(match.group(), "", 1)
+                print(match.group())
                 os.rename(os.path.join(path, filename), os.path.join(path, newname))
         print("All indexes removed.")
     except Exception as e:
@@ -74,15 +75,22 @@ def AddIndex(path="./", start_idx=1, end_idx=None):
 
 
 def Rename(path1: str, path2: str, start_idx=1, end_idx=None):
-    '''
+    """
     Parameters:
     path1 (str): The directory path to be processed.
     path2 (str): The directory path to be processed.
     start_idx (int, optional): The starting index. Defaults to 1.
     end_idx (int, optional): The ending index. If not provided, indexes will be added to all files.
-    '''
+    """
+
     Merge(path1, path2)
     RemoveIndex(os.path.join(path2, "input"))
     RemoveIndex(os.path.join(path2, "output"))
     AddIndex(os.path.join(path2, "input"), start_idx, end_idx)
     AddIndex(os.path.join(path2, "output"), start_idx, end_idx)
+
+    names1 = path1.split("_")
+    last_name1 = names1[-1]
+    names2 = path2.split("_")
+    last_name2 = names2[-1]
+    os.rename(path2, path2.replace(last_name2, str(last_name1 + last_name2)))
