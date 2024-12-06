0a8ee8752e4918bab078f76f444cc22552697ed4
Brian <108606033+Lenjis@users.noreply.github.com>
2024-12-06 20:36:24 +0800

    Update FastRename.py

--- FastRename.py
+++ FastRename.py
@@ -10,7 +10,6 @@ def Merge(path1: str, path2: str):
     Parameters:
     path (str, optional): The directory path to be processed.
     """
-    print("Merging files...")
     try:
         for filename in os.listdir(os.path.join(path1, "input")):
             shutil.move(
@@ -20,7 +19,6 @@ def Merge(path1: str, path2: str):
             shutil.move(
                 os.path.join(path1, "output", filename), os.path.join(path2, "output")
             )
-        print("Files merged.")
     except Exception as e:
         print(f"An error occurred: {e}")
 
@@ -30,15 +28,12 @@ def RemoveIndex(path="./"):
     Parameters:
     path (str, optional): The directory path to be processed. Defaults to the current directory.
     """
-    print("Removing index...")
     try:
         for filename in os.listdir(path):
             match = re.search(pattern, filename)
             if match:
                 newname = filename.replace(match.group(), "", 1)
-                print(match.group())
                 os.rename(os.path.join(path, filename), os.path.join(path, newname))
-        print("All indexes removed.")
     except Exception as e:
         print(f"An error occurred: {e}")
 
@@ -50,31 +45,24 @@ def AddIndex(path="./", start_idx=1, end_idx=None):
     end_idx (int, optional): The ending index. If not provided, indexes will be added to all files.
     """
     idx = start_idx
-    print("Adding index...")
     try:
         if end_idx == None:
             for filename in os.listdir(path):
-                match = re.search(pattern, filename)
-                if match:
-                    newname = str(idx) + filename
-                    os.rename(os.path.join(path, filename), os.path.join(path, newname))
-                    idx += 1
+                newname = str(idx) + filename
+                os.rename(os.path.join(path, filename), os.path.join(path, newname))
+                idx += 1
         else:
             for filename in os.listdir(path):
                 if idx <= end_idx:
-                    match = re.search(pattern, filename)
-                    if match:
-                        newname = str(idx) + filename
-                        os.rename(
-                            os.path.join(path, filename), os.path.join(path, newname)
-                        )
-                        idx += 1
-        print("Index added.")
+                    newname = str(idx) + filename
+                    os.rename(os.path.join(path, filename), os.path.join(path, newname))
+                    idx += 1
+        return idx
     except Exception as e:
         print(f"An error occurred: {e}")
 
 
-def Rename(path1: str, path2: str, start_idx=1, end_idx=None):
+def MergeAndRename(path1: str, path2: str, start_idx=1, end_idx=None):
     """
     Parameters:
     path1 (str): The directory path to be processed.
@@ -83,14 +71,20 @@ def Rename(path1: str, path2: str, start_idx=1, end_idx=None):
     end_idx (int, optional): The ending index. If not provided, indexes will be added to all files.
     """
 
+    print("Merging files...")
     Merge(path1, path2)
+    print("Removing the index...")
     RemoveIndex(os.path.join(path2, "input"))
     RemoveIndex(os.path.join(path2, "output"))
-    AddIndex(os.path.join(path2, "input"), start_idx, end_idx)
-    AddIndex(os.path.join(path2, "output"), start_idx, end_idx)
+    print("Re-adding the index...")
+    size1 = AddIndex(os.path.join(path2, "input"), start_idx, end_idx)
+    size2 = AddIndex(os.path.join(path2, "output"), start_idx, end_idx)
 
-    names1 = path1.split("_")
-    last_name1 = names1[-1]
-    names2 = path2.split("_")
-    last_name2 = names2[-1]
-    os.rename(path2, path2.replace(last_name2, str(last_name1 + last_name2)))
+    if size1 != size2:
+        print("The number of files in the input and output directories do not match!")
+        exit(1)
+    last_name = path2.split("_")[-1]
+    if last_name.isdigit():
+        os.rename(path2, path2.replace(last_name, str(size1 - 1)))
+    else:
+        os.rename(path2, path2 + "_" + str(size1 - 1))
